3a0af24b95df29aab439fea3a8097187a012f1e2
Carlos Rolando García Rocha <crgarcia@intelexion.com>
2019-02-14 19:59:58 -0600

    fix: se comenta linea de obsolete que causa problemas

--- src/Aqua.Newtonsoft.Json/PrimitiveValueInspector.cs
+++ src/Aqua.Newtonsoft.Json/PrimitiveValueInspector.cs
@@ -5,10 +5,10 @@ namespace Aqua
     using Aqua.TypeSystem;
     using System;
 
-    [Obsolete("Class was renamed to NativeValueInspector", true)]
+    // [Obsolete("Class was renamed to NativeValueInspector", true)]
     public static class PrimitiveValueInspector
     {
-        [Obsolete("Class was renamed to NativeValueInspector", true)]
+        // [Obsolete("Class was renamed to NativeValueInspector", true)]
         public static Func<object, object> GetConverter(TypeInfo typeInfo)
                 => NativeValueInspector.GetConverter(typeInfo);
     }
